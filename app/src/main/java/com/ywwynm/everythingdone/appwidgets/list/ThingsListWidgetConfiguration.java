6d0c5babc955e301a0ff9806539978506d55e7aa
ywwynm <ywwynm@gmail.com>
2016-08-10 15:45:42 +0800

    add configuration activity for things list

---
+++ app/src/main/java/com/ywwynm/everythingdone/appwidgets/list/ThingsListWidgetConfiguration.java
@@ -0,0 +1,40 @@
+package com.ywwynm.everythingdone.appwidgets.list;
+
+import com.ywwynm.everythingdone.activities.EverythingDoneBaseActivity;
+
+/**
+ * Created by qiizhang on 2016/8/10.
+ * Configuration Activity for things list widget
+ */
+public class ThingsListWidgetConfiguration extends EverythingDoneBaseActivity {
+
+    @Override
+    protected int getLayoutResource() {
+        return 0;
+    }
+
+    @Override
+    protected void initMembers() {
+
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void setActionbar() {
+
+    }
+
+    @Override
+    protected void setEvents() {
+
+    }
+}
