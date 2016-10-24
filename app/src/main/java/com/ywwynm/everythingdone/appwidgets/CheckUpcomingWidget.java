a79269680e07e120ff19de3431dc738052cc7e9f
ywwynm <ywwynm@gmail.com>
2016-10-24 21:03:58 +0800

    add check upcoming thing app widget reusing CreateWidget's layout

--- app/src/main/java/com/ywwynm/everythingdone/appwidgets/CheckUpcomingWidget.java
+++ app/src/main/java/com/ywwynm/everythingdone/appwidgets/CheckUpcomingWidget.java
@@ -7,15 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
-import com.ywwynm.everythingdone.App;
+import com.ywwynm.everythingdone.Def;
 import com.ywwynm.everythingdone.R;
-import com.ywwynm.everythingdone.activities.DetailActivity;
+import com.ywwynm.everythingdone.activities.ShortcutActivity;
 
 /**
- * Created by ywwynm on 2016/7/27.
- * App Widget for creating new thing
+ * Created by ywwynm on 2016/10/24.
+ * App Widget for checking upcoming thing
  */
-public class CreateWidget extends AppWidgetProvider {
+public class CheckUpcomingWidget extends AppWidgetProvider {
 
     public static final String TAG = "CreateWidget";
 
@@ -25,11 +25,14 @@ public class CreateWidget extends AppWidgetProvider {
             RemoteViews remoteViews = new RemoteViews(
                     context.getPackageName(), R.layout.app_widget_simple);
             remoteViews.setImageViewResource(
-                    R.id.iv_widget_simple, R.drawable.widget_create_content);
+                    R.id.iv_widget_simple, R.drawable.widget_check_upcoming_content);
             remoteViews.setContentDescription(
-                    R.id.iv_widget_simple, context.getString(R.string.title_create_thing));
+                    R.id.iv_widget_simple, context.getString(R.string.act_shortcut_check_upcoming));
 
-            Intent contentIntent = DetailActivity.getOpenIntentForCreate(context, TAG, App.newThingColor);
+            Intent contentIntent = new Intent(context, ShortcutActivity.class);
+            // Well, this is not very elegant but I don't want to change more code.
+            // And today is programmer's day, who cares about this?
+            contentIntent.setAction(Def.Communication.SHORTCUT_ACTION_CHECK_UPCOMING);
             PendingIntent pendingIntent = PendingIntent.getActivity(context,
                     appWidgetId, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
             remoteViews.setOnClickPendingIntent(R.id.iv_widget_simple, pendingIntent);
