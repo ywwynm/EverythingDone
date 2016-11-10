60a9c4fbabf831fc62a6d3616fdef86d593cd9e8
ywwynm <ywwynm@gmail.com>
2016-11-10 22:09:25 +0800

    determine ui for NotableNotificationActivity and integrate with other components

--- app/src/main/java/com/ywwynm/everythingdone/activities/NotableNotificationActivity.java
+++ app/src/main/java/com/ywwynm/everythingdone/activities/NotableNotificationActivity.java
@@ -1,7 +1,15 @@
 package com.ywwynm.everythingdone.activities;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,8 +18,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.ywwynm.everythingdone.App;
 import com.ywwynm.everythingdone.Def;
@@ -39,13 +47,15 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
 
     public static final String TAG = "NotableNotificationActivity";
 
+    public static final String BROADCAST_ACTION_JUST_FINISH = TAG + ".action.just_finish";
+
     public static Intent getOpenIntentForReminder(Context context, long thingId, int position) {
         return new Intent(context, NotableNotificationActivity.class)
                 .putExtra(Def.Communication.KEY_ID, thingId)
                 .putExtra(Def.Communication.KEY_POSITION, position);
     }
 
-    private static final String KEY_IS_HABIT = TAG + ".is_habit";
+    private static final String KEY_IS_HABIT = TAG + ".key.is_habit";
 
     public static Intent getOpenIntentForHabit(Context context, long hrId, int position, long hrTime) {
         return new Intent(context, NotableNotificationActivity.class)
@@ -66,12 +76,28 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
     private long mHrTime;
 
     private List<Integer> mActionsTexts;
+    private List<Integer> mActionsIcons;
     private List<View.OnClickListener> mActions;
 
     private RecyclerView mRvThing;
 
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (BROADCAST_ACTION_JUST_FINISH.equals(intent.getAction())) {
+                long thingId = intent.getLongExtra(Def.Communication.KEY_ID, -1L);
+                if (thingId == mThing.getId()) {
+                    finish();
+                }
+            }
+        }
+    };
+
     @Override
     protected void init() {
+        IntentFilter intentFilter = new IntentFilter(BROADCAST_ACTION_JUST_FINISH);
+        registerReceiver(mReceiver, intentFilter);
+
         initMembers();
         if (mThing != null) {
             findViews();
@@ -80,6 +106,18 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
             setEvents();
         } else {
             finish();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+        NotificationManagerCompat nmc = NotificationManagerCompat.from(this);
+        if (mIsHabit) {
+            nmc.cancel((int) mHrId);
+        } else {
+            nmc.cancel((int) mThing.getId());
         }
     }
 
@@ -117,6 +155,7 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
 
     private void initMemberActions() {
         mActionsTexts = new ArrayList<>();
+        mActionsIcons = new ArrayList<>();
         mActions = new ArrayList<>();
         @Thing.Type int thingType = mThing.getType();
         if (Thing.isReminderType(thingType)) {
@@ -127,7 +166,19 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
     }
 
     private void initMemberActionsReminder() {
+        if (mThing.getType() == Thing.REMINDER) {
+            mActionsTexts.add(R.string.act_start_doing);
+            mActionsIcons.add(R.drawable.act_start_doing);
+            mActions.add(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    sendBroadcastForReminderAndLeave(Def.Communication.NOTIFICATION_ACTION_START_DOING);
+                }
+            });
+        }
+
         mActionsTexts.add(R.string.act_finish);
+        mActionsIcons.add(R.drawable.act_finish);
         mActions.add(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -136,15 +187,8 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
         });
 
         if (mThing.getType() == Thing.REMINDER) {
-            mActionsTexts.add(R.string.act_start_doing);
-            mActions.add(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    sendBroadcastForReminderAndLeave(Def.Communication.NOTIFICATION_ACTION_START_DOING);
-                }
-            });
-
             mActionsTexts.add(R.string.act_delay);
+            mActionsIcons.add(R.drawable.act_delay);
             mActions.add(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -155,7 +199,23 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
     }
 
     private void initMemberActionsHabit() {
+        mActionsTexts.add(R.string.act_start_doing);
+        mActionsIcons.add(R.drawable.act_start_doing);
+        mActions.add(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotableNotificationActivity.this,
+                        HabitNotificationActionReceiver.class);
+                intent.setAction(Def.Communication.NOTIFICATION_ACTION_START_DOING);
+                intent.putExtra(Def.Communication.KEY_ID, mHrId);
+                intent.putExtra(Def.Communication.KEY_POSITION, mPosition);
+                sendBroadcast(intent);
+                finish();
+            }
+        });
+
         mActionsTexts.add(R.string.act_finish_this_time_habit);
+        mActionsIcons.add(R.drawable.act_finish);
         mActions.add(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -165,20 +225,6 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
                 intent.putExtra(Def.Communication.KEY_ID, mHrId);
                 intent.putExtra(Def.Communication.KEY_POSITION, mPosition);
                 intent.putExtra(Def.Communication.KEY_TIME, mHrTime);
-                sendBroadcast(intent);
-                finish();
-            }
-        });
-
-        mActionsTexts.add(R.string.act_start_doing);
-        mActions.add(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(NotableNotificationActivity.this,
-                        HabitNotificationActionReceiver.class);
-                intent.setAction(Def.Communication.NOTIFICATION_ACTION_START_DOING);
-                intent.putExtra(Def.Communication.KEY_ID, mHrId);
-                intent.putExtra(Def.Communication.KEY_POSITION, mPosition);
                 sendBroadcast(intent);
                 finish();
             }
@@ -251,6 +297,17 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
                 holder.vReminderSeparator.setVisibility(View.GONE);
                 holder.vHabitSeparator1.setVisibility(View.GONE);
                 holder.flDoing.setVisibility(View.GONE);
+
+                holder.cv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final Intent intent = DetailActivity.getOpenIntentForUpdate(
+                                NotableNotificationActivity.this, NotableNotificationActivity.TAG,
+                                mThing.getId(), mPosition);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
             }
         };
         mRvThing.setLayoutManager(new LinearLayoutManager(this));
@@ -259,19 +316,46 @@ public class NotableNotificationActivity extends EverythingDoneBaseActivity {
 
     private void initActionsUI() {
         float density = DisplayUtil.getScreenDensity(this);
-        int p12 = (int) (density * 12);
-        int p8 = (int) (density * 8);
-        int textColor = ContextCompat.getColor(this, R.color.app_accent);
+        int dp40 = (int) (density * 40);
+        int dp8 = (int) (density * 8);
+        int dp2 = (int) (density * 2);
+        int accentColor = ContextCompat.getColor(this, R.color.app_accent);
+        int black_26p = ContextCompat.getColor(this, R.color.black_26p);
+        int black_54p = ContextCompat.getColor(this, R.color.black_54p);
         final int size = mActions.size();
         LinearLayout ll = f(R.id.ll_actions_notable_notification);
         for (int i = 0; i < size; i++) {
-            TextView tvAsBt = new TextView(this);
-            tvAsBt.setPaddingRelative(p12, p8, p12, p8);
-            tvAsBt.setText(mActionsTexts.get(i));
-            tvAsBt.setTextColor(textColor);
-            tvAsBt.setOnClickListener(mActions.get(i));
-            tvAsBt.setBackgroundResource(R.drawable.selectable_item_background_light);
-            ll.addView(tvAsBt);
+//            TextView tvAsBt = new TextView(this);
+//            tvAsBt.setPaddingRelative(p12, p8, p12, p8);
+//            tvAsBt.setText(mActionsTexts.get(i));
+//            tvAsBt.setTextColor(textColor);
+//            tvAsBt.setOnClickListener(mActions.get(i));
+//            tvAsBt.setBackgroundResource(R.drawable.selectable_item_background_light);
+//            ll.addView(tvAsBt);
+
+            FloatingActionButton fab = new FloatingActionButton(this);
+            if (i == 0) {
+                fab.setBackgroundTintList(ColorStateList.valueOf(accentColor));
+            } else {
+                fab.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+            }
+            fab.setRippleColor(black_26p);
+
+            fab.setCompatElevation(dp2);
+
+            Drawable drawable = ContextCompat.getDrawable(this, mActionsIcons.get(i));
+            drawable = drawable.mutate();
+            drawable.setColorFilter(black_54p, PorterDuff.Mode.SRC_ATOP);
+            fab.setScaleType(ImageView.ScaleType.CENTER);
+            fab.setImageDrawable(drawable);
+
+            fab.setOnClickListener(mActions.get(i));
+
+            fab.setContentDescription(getString(mActionsTexts.get(i)));
+
+            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(dp40, dp40);
+            llp.setMargins(dp8, dp8, dp8, dp8);
+            ll.addView(fab, llp);
         }
     }
 
