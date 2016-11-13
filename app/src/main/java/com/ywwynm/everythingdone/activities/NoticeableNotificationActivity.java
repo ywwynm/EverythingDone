3cabb7244fa7c722cc31cb4e75f1412da9db2da5
ywwynm <ywwynm@gmail.com>
2016-11-13 14:51:48 +0800

    change ui for NoticeableNotificationActivity

--- app/src/main/java/com/ywwynm/everythingdone/activities/NoticeableNotificationActivity.java
+++ app/src/main/java/com/ywwynm/everythingdone/activities/NoticeableNotificationActivity.java
@@ -1,5 +1,6 @@
 package com.ywwynm.everythingdone.activities;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,17 +8,21 @@ import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ywwynm.everythingdone.App;
 import com.ywwynm.everythingdone.Def;
@@ -32,6 +37,8 @@ import com.ywwynm.everythingdone.receivers.ReminderNotificationActionReceiver;
 import com.ywwynm.everythingdone.utils.DeviceUtil;
 import com.ywwynm.everythingdone.utils.DisplayUtil;
 import com.ywwynm.everythingdone.utils.EdgeEffectUtil;
+
+import org.joda.time.DateTime;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -77,9 +84,13 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
     private List<Integer> mActionsIcons;
     private List<View.OnClickListener> mActions;
 
+    private ImageView mIvTitle;
+    private TextView mTvTitle;
+
     private RecyclerView mRvThing;
 
-    private FloatingActionButton[] mFabs;
+    private FrameLayout[] mFlActions;
+    private ImageView[] mIvActions;
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -166,17 +177,6 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
     }
 
     private void initMemberActionsReminder() {
-        if (mThing.getType() == Thing.REMINDER) {
-            mActionsTexts.add(R.string.act_start_doing);
-            mActionsIcons.add(R.drawable.act_start_doing);
-            mActions.add(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    sendBroadcastForReminderAndLeave(Def.Communication.NOTIFICATION_ACTION_START_DOING);
-                }
-            });
-        }
-
         mActionsTexts.add(R.string.act_finish);
         mActionsIcons.add(R.drawable.act_finish);
         mActions.add(new View.OnClickListener() {
@@ -187,6 +187,15 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
         });
 
         if (mThing.getType() == Thing.REMINDER) {
+            mActionsTexts.add(R.string.act_start_doing);
+            mActionsIcons.add(R.drawable.act_start_doing);
+            mActions.add(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    sendBroadcastForReminderAndLeave(Def.Communication.NOTIFICATION_ACTION_START_DOING);
+                }
+            });
+
             mActionsTexts.add(R.string.act_delay);
             mActionsIcons.add(R.drawable.act_delay);
             mActions.add(new View.OnClickListener() {
@@ -199,21 +208,6 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
     }
 
     private void initMemberActionsHabit() {
-        mActionsTexts.add(R.string.act_start_doing);
-        mActionsIcons.add(R.drawable.act_start_doing);
-        mActions.add(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(NoticeableNotificationActivity.this,
-                        HabitNotificationActionReceiver.class);
-                intent.setAction(Def.Communication.NOTIFICATION_ACTION_START_DOING);
-                intent.putExtra(Def.Communication.KEY_ID, mHrId);
-                intent.putExtra(Def.Communication.KEY_POSITION, mPosition);
-                sendBroadcast(intent);
-                finish();
-            }
-        });
-
         mActionsTexts.add(R.string.act_finish_this_time_habit);
         mActionsIcons.add(R.drawable.act_finish);
         mActions.add(new View.OnClickListener() {
@@ -225,6 +219,21 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
                 intent.putExtra(Def.Communication.KEY_ID, mHrId);
                 intent.putExtra(Def.Communication.KEY_POSITION, mPosition);
                 intent.putExtra(Def.Communication.KEY_TIME, mHrTime);
+                sendBroadcast(intent);
+                finish();
+            }
+        });
+
+        mActionsTexts.add(R.string.act_start_doing);
+        mActionsIcons.add(R.drawable.act_start_doing);
+        mActions.add(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NoticeableNotificationActivity.this,
+                        HabitNotificationActionReceiver.class);
+                intent.setAction(Def.Communication.NOTIFICATION_ACTION_START_DOING);
+                intent.putExtra(Def.Communication.KEY_ID, mHrId);
+                intent.putExtra(Def.Communication.KEY_POSITION, mPosition);
                 sendBroadcast(intent);
                 finish();
             }
@@ -242,12 +251,20 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
 
     @Override
     protected void findViews() {
+        mTvTitle = f(R.id.tv_noticeable_notification_title);
+        mIvTitle = f(R.id.iv_noticeable_notification_title);
+
         mRvThing = f(R.id.rv_thing_noticeable_notification);
 
-        mFabs = new FloatingActionButton[3];
-        mFabs[0] = f(R.id.fab_1_noticeable_notification);
-        mFabs[1] = f(R.id.fab_2_noticeable_notification);
-        mFabs[2] = f(R.id.fab_3_noticeable_notification);
+        mFlActions = new FrameLayout[3];
+        mFlActions[0] = f(R.id.fl_1_noticeable_notification_as_bt);
+        mFlActions[1] = f(R.id.fl_2_noticeable_notification_as_bt);
+        mFlActions[2] = f(R.id.fl_3_noticeable_notification_as_bt);
+
+        mIvActions = new ImageView[3];
+        mIvActions[0] = f(R.id.iv_1_noticeable_notification_as_bt);
+        mIvActions[1] = f(R.id.iv_2_noticeable_notification_as_bt);
+        mIvActions[2] = f(R.id.iv_3_noticeable_notification_as_bt);
     }
 
     @Override
@@ -256,8 +273,32 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
         wlp.width = mDialogWidth;
         getWindow().setAttributes(wlp);
 
+        initTitleUI();
         initRvThing();
         initActionsUI();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void initTitleUI() {
+        @Thing.Type int thingType = mThing.getType();
+        int iconRes = Thing.getTypeIconWhiteLarge(thingType);
+        Drawable d1 = ContextCompat.getDrawable(this, iconRes);
+        Drawable d2 = d1.mutate();
+        d2.setColorFilter(mThing.getColor(), PorterDuff.Mode.SRC_ATOP);
+        mIvTitle.setImageDrawable(d2);
+
+        String typeStr = Thing.getTypeStr(thingType, this);
+        mIvTitle.setContentDescription(typeStr);
+
+        String timeStr = new DateTime().toString("HH:mm");
+        String title = typeStr + " • " + timeStr;
+        SpannableStringBuilder ssb = new SpannableStringBuilder(title);
+        ForegroundColorSpan colorSpan1 = new ForegroundColorSpan(mThing.getColor());
+        ForegroundColorSpan colorSpan2 = new ForegroundColorSpan(Color.parseColor("#66000000"));
+        int index = title.indexOf('•');
+        ssb.setSpan(colorSpan1, 0, index - 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(colorSpan2, index - 1, title.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mTvTitle.setText(ssb);
     }
 
     private void initRvThing() {
@@ -276,9 +317,10 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
             @Override
             public void onBindViewHolder(BaseThingViewHolder holder, int position) {
                 super.onBindViewHolder(holder, position);
-                holder.cv.setCardBackgroundColor(Color.WHITE);
+                holder.cv.setCardBackgroundColor(Color.parseColor("#F6F6F6"));
                 holder.cv.setRadius(0);
                 holder.cv.setCardElevation(0);
+
                 holder.tvTitle.setMaxLines(Integer.MAX_VALUE);
                 holder.tvContent.setMaxLines(Integer.MAX_VALUE);
                 holder.rlReminder.setVisibility(View.GONE);
@@ -286,6 +328,7 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
                 holder.vReminderSeparator.setVisibility(View.GONE);
                 holder.vHabitSeparator1.setVisibility(View.GONE);
                 holder.flDoing.setVisibility(View.GONE);
+                holder.ivSticky.setVisibility(View.GONE);
 
                 holder.cv.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -307,23 +350,17 @@ public class NoticeableNotificationActivity extends EverythingDoneBaseActivity {
     }
 
     private void initActionsUI() {
-        int black_54p = ContextCompat.getColor(this, R.color.black_54p);
+        int iconColor = DisplayUtil.getTransparentColor(mThing.getColor(), 224);
+//        int iconColor = mThing.getColor();
         final int size = mActions.size();
         for (int i = 0; i < size; i++) {
-            mFabs[i].setVisibility(View.VISIBLE);
-
+            mFlActions[i].setVisibility(View.VISIBLE);
             Drawable drawable = ContextCompat.getDrawable(this, mActionsIcons.get(i));
             drawable = drawable.mutate();
-            drawable.setColorFilter(black_54p, PorterDuff.Mode.SRC_ATOP);
-            mFabs[i].setScaleType(ImageView.ScaleType.CENTER);
-            mFabs[i].setImageDrawable(drawable);
-
-            mFabs[i].setOnClickListener(mActions.get(i));
-            mFabs[i].setContentDescription(getString(mActionsTexts.get(i)));
-        }
-
-        for (int i = size; i < 3; i++) {
-            mFabs[i].setVisibility(View.GONE);
+            drawable.setColorFilter(iconColor, PorterDuff.Mode.SRC_ATOP);
+            mIvActions[i].setImageDrawable(drawable);
+            mFlActions[i].setOnClickListener(mActions.get(i));
+            mIvActions[i].setContentDescription(getString(mActionsTexts.get(i)));
         }
     }
 
