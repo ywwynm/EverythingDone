4865196718ea3004c81e1e6d92086bcf3edb9565
ywwynm <ywwynm@gmail.com>
2016-11-15 09:00:03 +0800

    fix for findbugs

--- app/src/main/java/com/ywwynm/everythingdone/utils/StringUtil.java
+++ app/src/main/java/com/ywwynm/everythingdone/utils/StringUtil.java
@@ -1,5 +1,7 @@
 package com.ywwynm.everythingdone.utils;
 
+import android.text.TextUtils;
+
 /**
  * Created by ywwynm on 2016/10/20.
  * some utils to operate String
@@ -11,11 +13,17 @@ public class StringUtil {
     private StringUtil() {}
 
     public static String upperFirst(String s) {
+        if (TextUtils.isEmpty(s)) {
+            return "";
+        }
         String first = String.valueOf(s.charAt(0));
         return first.toUpperCase() + s.substring(1, s.length());
     }
 
     public static String lowerFirst(String s) {
+        if (TextUtils.isEmpty(s)) {
+            return "";
+        }
         String first = String.valueOf(s.charAt(0));
         return first.toLowerCase() + s.substring(1, s.length());
     }
