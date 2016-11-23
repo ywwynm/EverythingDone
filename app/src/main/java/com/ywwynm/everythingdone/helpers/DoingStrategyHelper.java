c9624bf5b1f91af9343d0673d82941f0319b9a02
ywwynm <ywwynm@gmail.com>
2016-11-23 14:33:57 +0800

    implements methods for auto strict mode

--- app/src/main/java/com/ywwynm/everythingdone/helpers/DoingStrategyHelper.java
+++ app/src/main/java/com/ywwynm/everythingdone/helpers/DoingStrategyHelper.java
@@ -26,6 +26,18 @@ public class DoingStrategyHelper {
     public static int SYS_AUTO_START_DOING_STRATEGY_HABIT       = 2;
     public static int SYS_AUTO_START_DOING_STRATEGY_ALL         = 3;
 
+
+    public static int KEY_INDEX_AUTO_STRICT_MODE                = 1;
+
+    public static int AUTO_STRICT_MODE_STRATEGY_FOLLOW_SETTINGS = 0;
+    public static int AUTO_STRICT_MODE_STRATEGY_ENABLED         = 1;
+    public static int AUTO_STRICT_MODE_STRATEGY_DISABLED        = 2;
+
+    public static int SYS_AUTO_STRICT_MODE_STRATEGY_DISABLED    = 0;
+    public static int SYS_AUTO_STRICT_MODE_STRATEGY_REMINDER    = 1;
+    public static int SYS_AUTO_STRICT_MODE_STRATEGY_HABIT       = 2;
+    public static int SYS_AUTO_STRICT_MODE_STRATEGY_ALL         = 3;
+
     private Context mContext;
     private SharedPreferences mSpStartDoing;
     private SharedPreferences mSpSettings;
@@ -39,12 +51,12 @@ public class DoingStrategyHelper {
     }
 
     /**
-     * Get auto start doing strategy for a Thing with given id
-     * @param thingId Thing's id
+     * Get auto start doing strategy for a thing with given id
+     * @param thingId thing's id
      * @return auto start doing strategy for this thing, should be one of:
      *      {@link #AUTO_START_DOING_STRATEGY_FOLLOW_SETTINGS},
      *      {@link #AUTO_START_DOING_STRATEGY_DISABLED},
-     *      {@link #AUTO_START_DOING_STRATEGY_ENABLED}
+     *      {@link #AUTO_START_DOING_STRATEGY_ENABLED}.
      */
     public int getAutoStartDoingStrategy(long thingId) {
         String key = thingId + "_" + KEY_INDEX_AUTO_START_DOING;
@@ -52,11 +64,11 @@ public class DoingStrategyHelper {
     }
 
     /**
-     * Judge if user should start doing a Thing automatically when its alarm ring.
-     * This method will consider both general settings and unique settings for that Thing.
-     * @param thingId Thing's id
-     * @return {@code true} if user should start doing the Thing when its alarm ring.
-     *         {@code false} otherwise
+     * Judge if user should start doing the thing automatically when its alarm ring.
+     * This method will consider both general settings and unique settings for that thing.
+     * @param thingId thing's id
+     * @return {@code true} if user should start doing the thing when its alarm ring.
+     *         {@code false} otherwise.
      */
     public boolean shouldAutoStartDoing(long thingId) {
         int strategy = getAutoStartDoingStrategy(thingId);
@@ -82,11 +94,60 @@ public class DoingStrategyHelper {
             }
         } else if (strategy == AUTO_START_DOING_STRATEGY_ENABLED) {
             return true;
-        } else return false;
+        } else { // AUTO_START_DOING_STRATEGY_DISABLED
+            return false;
+        }
     }
 
+
+    /**
+     * Get auto strict mode strategy for the thing with given id
+     * @param thingId thing's id
+     * @return auto start doing strategy for this thing, should be one of:
+     *      {@link #AUTO_STRICT_MODE_STRATEGY_FOLLOW_SETTINGS},
+     *      {@link #AUTO_STRICT_MODE_STRATEGY_DISABLED},
+     *      {@link #AUTO_STRICT_MODE_STRATEGY_ENABLED}.
+     */
+    public int getAutoStrictModeStrategy(long thingId) {
+        String key = thingId + "_" + KEY_INDEX_AUTO_STRICT_MODE;
+        return mSpStartDoing.getInt(key, AUTO_STRICT_MODE_STRATEGY_FOLLOW_SETTINGS);
+    }
+
+
+    /**
+     * Judge if strict mode should be turned on automatically when user starts doing the thing.
+     * This method will consider both general settings and unique settings for that thing.
+     * @param thingId thing's id
+     * @return {@code true} if strict mode should be turned on automatically when user starts doing
+     *         the thing. {@code false} otherwise.
+     */
     public boolean shouldAutoStrictMode(long thingId) {
-        throw new UnsupportedOperationException();
+        int strategy = getAutoStrictModeStrategy(thingId);
+        if (strategy == AUTO_STRICT_MODE_STRATEGY_FOLLOW_SETTINGS) {
+            int sysStrategy = mSpSettings.getInt(Def.Meta.KEY_AUTO_STRICT_MODE,
+                    SYS_AUTO_STRICT_MODE_STRATEGY_DISABLED);
+            if (sysStrategy == SYS_AUTO_STRICT_MODE_STRATEGY_DISABLED) {
+                return false;
+            } else if (sysStrategy == SYS_AUTO_STRICT_MODE_STRATEGY_ALL) {
+                // suppose that this method will only be called for Reminder or Habit
+                return true;
+            } else {
+                Thing thing = ThingDAO.getInstance(mContext).getThingById(thingId);
+                if (thing == null) {
+                    return false;
+                }
+                @Thing.Type int thingType = thing.getType();
+                if (thingType == Thing.REMINDER && sysStrategy == SYS_AUTO_STRICT_MODE_STRATEGY_REMINDER) {
+                    return true;
+                } else if (thingType == Thing.HABIT && sysStrategy == SYS_AUTO_STRICT_MODE_STRATEGY_HABIT) {
+                    return true;
+                } else return false;
+            }
+        } else if (strategy == AUTO_STRICT_MODE_STRATEGY_ENABLED) {
+            return true;
+        } else { // AUTO_STRICT_MODE_STRATEGY_DISABLED
+            return false;
+        }
     }
 
     public long getAutoDoingTime(long thingId) {
