e43eea2f2f17e9eb2ed935847abcdef32fb2061b
ywwynm <ywwynm@gmail.com>
2016-11-23 09:26:45 +0800

    fix: will show reminded or expired Reminder when checking upcoming thing

--- app/src/main/java/com/ywwynm/everythingdone/activities/ShortcutActivity.java
+++ app/src/main/java/com/ywwynm/everythingdone/activities/ShortcutActivity.java
@@ -9,7 +9,9 @@ import android.widget.Toast;
 import com.ywwynm.everythingdone.App;
 import com.ywwynm.everythingdone.Def;
 import com.ywwynm.everythingdone.R;
+import com.ywwynm.everythingdone.database.ReminderDAO;
 import com.ywwynm.everythingdone.database.ThingDAO;
+import com.ywwynm.everythingdone.model.Reminder;
 import com.ywwynm.everythingdone.model.Thing;
 import com.ywwynm.everythingdone.utils.ThingsSorter;
 
@@ -34,12 +36,24 @@ public class ShortcutActivity extends AppCompatActivity {
             openIntent = DetailActivity.getOpenIntentForCreate(
                     this, TAG, App.newThingColor);
         } else if (Def.Communication.SHORTCUT_ACTION_CHECK_UPCOMING.equals(action)) {
+            boolean canCheck = false;
             List<Thing> things = ThingDAO.getInstance(this)
                     .getThingsForDisplay(Def.LimitForGettingThings.ALL_UNDERWAY);
             Collections.sort(things, ThingsSorter.getThingComparatorByAlarmTime(true));
             Thing thing = things.get(1); // 0 is header
             @Thing.Type int thingType = thing.getType();
-            if (Thing.isReminderType(thingType) || thingType == Thing.HABIT) {
+            if (thingType == Thing.HABIT) {
+                canCheck = true;
+            } else if (Thing.isReminderType(thingType)) {
+                Reminder reminder = ReminderDAO.getInstance(this).getReminderById(thing.getId());
+                if (reminder == null || reminder.getState() != Reminder.UNDERWAY) {
+                    canCheck = false;
+                } else {
+                    canCheck = true;
+                }
+            }
+
+            if (canCheck) {
                 openIntent = AuthenticationActivity.getOpenIntent(
                         this, TAG, thing.getId(), -1,
                         Def.Communication.AUTHENTICATE_ACTION_VIEW,
